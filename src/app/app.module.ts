8d7db2e389e6f1d3714bf8487d9d6f6708a4f7d3
Mark Handy <mjhandy@outlook.com>
2023-07-21 13:49:57 -0300

    Initial reactive form, with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,11 @@ import { BrowserModule, Meta } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+// custom components
+import { CarouselComponent } from './components/pages/carousel/carousel.component';
+import { AccordionComponent } from './components/pages/accordion/accordion.component';
 import { HeaderComponent } from './components/header/header/header.component';
 import { MainNavComponent } from './components/header/main-nav/main-nav.component';
 import { GlobalSearchComponent } from './components/header/global-search/global-search.component';
@@ -17,11 +20,10 @@ import { AboutUsComponent } from './components/pages/about-us/about-us.component
 import { OneColumnTemplateComponent } from './components/pages/one-column-template/one-column-template.component';
 import { TwoColumnTemplateSideRailComponent } from './components/pages/two-column-template-side-rail/two-column-template-side-rail.component';
 
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// custom components
-import { CarouselComponent } from './components/pages/carousel/carousel.component';
-import { AccordionComponent } from './components/pages/accordion/accordion.component';
+
 
 // material items
 import { MaterialModule } from './shared/material/material.module';
@@ -51,7 +53,9 @@ import { FormsComponent } from './components/pages/forms/forms.component';
     BrowserAnimationsModule,
     SvgIconsModule,
     CarouselComponent,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [Meta],
   bootstrap: [
--- src/app/components/pages/forms/forms.component.ts
+++ src/app/components/pages/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
+
+  form = new FormGroup({
+    fName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
+
+  get f(){
+    return this.form.controls;
+  }
+
+
+
+  onSubmit(){
+    console.debug(this.form.value);
+  }
 
 }
